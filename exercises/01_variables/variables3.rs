64043ca9e3c719cec90f34a0028ebf240063817f
V1337Q <clutsoo@gmail.com>
2025-04-05 03:12:34 -0400

    Added clearer exp. on variables3

--- exercises/01_variables/variables3.rs
+++ exercises/01_variables/variables3.rs
@@ -1,7 +1,15 @@
 fn main() {
     // TODO: Change the line below to fix the compiler error.
     let x: i32 = 10;
-    // Missing value on variable
-
     println!("Number {x}");
+    // Missing value on variable
+    
+    //: i32 is for declaring a file type explicitly.
+    
+    // The other expression would be:
+    let y: i32;
+    y = 5;
+    println!("y: {}", y);
+        
+
 }
