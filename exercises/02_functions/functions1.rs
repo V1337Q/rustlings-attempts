eb8a0199740a6a75766b7b2ce20db3200c25e5a8
V1337Q <clutsoo@gmail.com>
2025-04-05 03:18:56 -0400

    Finishes function1

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,4 +1,8 @@
 // TODO: Add some function with the name `call_me` without arguments or a return value.
+fn call_me(){
+    println!("Call me!");
+}
+//The instruction are clear enough.
 
 fn main() {
     call_me(); // Don't change this line
