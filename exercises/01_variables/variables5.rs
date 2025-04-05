e967fde455baf6bc3f8dd9e277055b960e4aae89
V1337Q <clutsoo@gmail.com>
2025-04-05 03:14:23 -0400

    Finishes variables5

--- exercises/01_variables/variables5.rs
+++ exercises/01_variables/variables5.rs
@@ -3,6 +3,8 @@ fn main() {
     println!("Spell a number: {}", number);
 
     // TODO: Fix the compiler error by changing the line below without renaming the variable.
-    number = 3;
+    let number = 3;
+    // Re-declaring the variable 'number' which causes what we call Variable Shadowing.
+    // Coulda just comment the last two lines though. Haha.
     println!("Number plus two is: {}", number + 2);
 }
