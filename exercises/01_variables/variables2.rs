28e03d4d75819dd30e06d692f1a76b1b04f4a9ab
V1337Q <clutsoo@gmail.com>
2025-04-05 03:07:00 -0400

    Finishes variables1 & 2

--- exercises/01_variables/variables2.rs
+++ exercises/01_variables/variables2.rs
@@ -1,6 +1,7 @@
 fn main() {
     // TODO: Change the line below to fix the compiler error.
-    let x;
+    let x = 10;
+    // Missing value on variable
 
     if x == 10 {
         println!("x is ten!");
